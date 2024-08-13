059533abaf61be43a21204ec9deda11144475da6
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-13 18:48:18 -0500

    feat: add the better usage for practices

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -3,7 +3,13 @@
 ---------------------------------------------
 
 return {
-  --- Hard time
+  --- Hard time for make a better practice and usage
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    lazy = false,
+    config = require('plugins.configs.hardtimec'),
+  },
 
   --- Treesitter
 }
