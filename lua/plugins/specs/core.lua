8b874889dcc0b09fc4e5c93cd1ccc38204ac6bca
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-14 22:15:08 -0500

    feat: add treesiter and tsautotag

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -12,4 +12,21 @@ return {
   },
 
   --- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ':TSUpdate',
+    -- event = { 'LazyFile', 'VeryLazy' },
+    event = { 'VeryLazy' },
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
+    config = require('plugins.configs.treesitterc'),
+  },
 }
