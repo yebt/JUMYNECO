dfc2289f5d55890b0a0354b75ee9aea422bf715d
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-02 11:38:16 -0500

    feat: move the specs of plugins to specs folder

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,7 +45,7 @@ require('lazy').setup({
 
   spec = {
     -- import your plugins
-    { import = 'plugins' },
+    { import = 'plugins.specs' },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
