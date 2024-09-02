7670a90c52aaa7190e249e7828ad10829fb18e22
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-02 14:22:40 -0500

    feat: add snipe to swith bufers

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -1,4 +1,49 @@
 return {
+
+  --- Buffers
+  {
+    'leath-dub/snipe.nvim',
+    keys = {
+      {
+        'gb',
+        function()
+          require('snipe').open_buffer_menu()
+        end,
+        desc = 'Open Snipe buffer menu',
+      },
+    },
+    opts = {
+      ui = {
+        max_width = -1, -- -1 means dynamic width
+        -- Where to place the ui window
+        -- Can be any of "topleft", "bottomleft", "topright", "bottomright", "center", "cursor" (sets under the current cursor pos)
+        position = 'topleft',
+      },
+      hints = {
+        -- Charaters to use for hints (NOTE: make sure they don't collide with the navigation keymaps)
+        dictionary = 'sadflewcmpghio',
+      },
+      navigate = {
+        -- When the list is too long it is split into pages
+        -- `[next|prev]_page` options allow you to navigate
+        -- this list
+        next_page = 'J',
+        prev_page = 'K',
+
+        -- You can also just use normal navigation to go to the item you want
+        -- this option just sets the keybind for selecting the item under the
+        -- cursor
+        under_cursor = '<cr>',
+
+        -- In case you changed your mind, provide a keybind that lets you
+        -- cancel the snipe and close the window.
+        cancel_snipe = '<esc>',
+      },
+      -- Define the way buffers are sorted by default
+      -- Can be any of "default" (sort buffers by their number) or "last" (sort buffers by last accessed)
+      sort = 'last',
+    },
+  },
   --- Iterator
   {
     'folke/trouble.nvim',
@@ -14,6 +59,7 @@ return {
     cmd = {
       'RenderMarkdown',
     },
+    config = require('plugins.configs.rendermarkdown'),
   },
 
   --- Keys
