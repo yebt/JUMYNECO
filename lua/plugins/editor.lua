ba874a4f4f6166573348334b9ecc9d7244992816
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-02 11:12:48 -0500

    feat: change the picker by minipicker and usage of sessions with minisessions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -130,4 +130,79 @@ return {
       { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
     },
   },
+
+  --- mini sessions
+  {
+    'echasnovski/mini.sessions',
+    version = false,
+    -- lazy = false,
+    -- event = 'VeryLazy',
+    init = function()
+      -- Make autosave session
+      vim.api.nvim_create_autocmd('VimLeavePre', {
+        group = vim.api.nvim_create_augroup('_mini_session_persistence', { clear = true }),
+        callback = function()
+          local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+          local cntrs = #buffers
+
+          for _, buffer in ipairs(buffers) do
+            if buffer.name == '' then
+              cntrs = cntrs - 1
+            end
+          end
+
+          if cntrs ~= 0 then
+            local cwd = vim.fn.getcwd()
+            local basename = cwd:match('[^/]+$')
+            local path = cwd:gsub('/', '%%')
+            local session_name = basename .. ' (' .. path .. ')'
+            local session_opts = { verbose = false }
+            if MiniSessions then
+              MiniSessions.write(session_name)
+              MiniSessions.write('latest') -- save the latest work
+            end
+          end
+        end,
+      })
+    end,
+    keys = {
+      -- {
+      --   "<leader>ST",
+      --   function()
+      --     vim.notify(vim.inspect(MiniSessions.detected))
+      --   end,
+      -- },
+      {
+        '<leader>SS',
+        function()
+          MiniSessions.select()
+        end,
+        desc = 'Select a session',
+      },
+      {
+        '<leader>SM',
+        function()
+          -- local session_name = ('%s%ssession'):format(vim.fn.stdpath('data'), '_')
+          local cwd = vim.fn.getcwd()
+          local basename = cwd:match('[^/]+$')
+          local path = cwd:gsub('/', '%%')
+          -- local session_name = vim.fn.getcwd():gsub(pattern, "%%")
+          -- local session_name = basename .. " (" .. cwd .. ")"
+          local session_name = basename .. ' (' .. path .. ')'
+          -- vim.notify(vim.inspect(session_name))
+          MiniSessions.write(session_name)
+        end,
+        desc = 'Make session',
+      },
+      {
+        '<leader>SL',
+        function()
+          MiniSessions.read('latest')
+        end,
+        desc = "Read 'latest' session",
+      },
+    },
+
+    config = require('plugins.configs.minisessions'),
+  },
 }
--- lua/plugins/pickers.lua
+++ lua/plugins/pickers.lua
@@ -1,54 +1,121 @@
 return {
   --- Telescope
-  -- Telescope
+  -- {
+  --   'nvim-telescope/telescope.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --   },
+  --   cmd = 'Telescope',
+  --   keys = {
+  --     {
+  --       '<leader>T<space>',
+  --       ':Telescope<CR>',
+  --       desc = 'Open telescope',
+  --     },
+  --     {
+  --       '<C-p>',
+  --       function()
+  --         require('telescope.builtin').find_files({
+  --           prompt_prefix = ' ',
+  --           prompt_title = '',
+  --           -- selection_caret = "* ",
+  --           multi_icon = '- ',
+  --           attach_mappings = function(_, map)
+  --             map('i', '<ESC>', require('telescope.actions').close)
+  --             return true
+  --           end,
+  --           results_title = false,
+  --           sorting_strategy = 'ascending',
+  --           layout_strategy = 'center',
+  --           layout_config = {
+  --             -- preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+  --             -- preview_cutoff = 0, -- Preview should always show (unless previewer = false)
+  --             -- width = 0.5,
+  --             width = 70,
+  --             -- height = 12,
+  --             -- preview_cutoff = 120,
+  --             anchor = 'N',
+  --           },
+  --           -- -- border = false,
+  --           border = true,
+  --           borderchars = {
+  --             prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
+  --             results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
+  --             preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+  --           },
+  --         })
+  --       end,
+  --       silent = true,
+  --       desc = 'Telescope fast ff',
+  --     },
+  --   },
+  -- },
+
+  --- Picker
+
   {
-    'nvim-telescope/telescope.nvim',
+    'echasnovski/mini.pick',
+    version = false,
+    cmd = { 'Pick' },
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      { 'echasnovski/mini.extra', version = false },
     },
-    cmd = 'Telescope',
     keys = {
-      {
-        '<leader>T<space>',
-        ':Telescope<CR>',
-        desc = 'Open telescope',
-      },
       {
         '<C-p>',
         function()
-          require('telescope.builtin').find_files({
-            prompt_prefix = ' ',
-            prompt_title = '',
-            -- selection_caret = "* ",
-            multi_icon = '- ',
-            attach_mappings = function(_, map)
-              map('i', '<ESC>', require('telescope.actions').close)
-              return true
-            end,
-            results_title = false,
-            sorting_strategy = 'ascending',
-            layout_strategy = 'center',
-            layout_config = {
-              -- preview_cutoff = 1, -- Preview should always show (unless previewer = false)
-              -- preview_cutoff = 0, -- Preview should always show (unless previewer = false)
-              -- width = 0.5,
-              width = 70,
-              -- height = 12,
-              -- preview_cutoff = 120,
-              anchor = 'N',
-            },
-            -- -- border = false,
-            border = true,
-            borderchars = {
-              prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
-              results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
-              preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-            },
-          })
+          MiniPick.builtin.files({ tool = 'rg' })
         end,
-        silent = true,
-        desc = 'Telescope fast ff',
+
+        desc = 'Search the fiels in path',
+      },
+      {
+        '<leader>b',
+        function()
+          if MiniPick.registry.bufferlist then
+            MiniPick.registry.bufferlist()
+          end
+        end,
+
+        desc = 'Pick orderer buffers',
       },
     },
+    init = function()
+      local au = vim.api.nvim_create_autocmd
+      local function augroup(name)
+        return vim.api.nvim_create_augroup('_kernel_' .. name, { clear = true })
+      end
+
+      vim.ui.select = function(...)
+        vim.ui.select = require('mini.pick').ui_select
+        return vim.ui.select(...)
+      end
+
+      -- vim.keymap.set('n', '<C-b>', function()
+      --   local bfs = _G._kernel._bufferslist
+      --   vim.notify(vim.inspect(bfs or 'No BL'))
+      -- end, { desc = 'Print the list of buffers' })
+
+      -- Init the list
+      if not vim.g._bufferslist then
+        vim.g._bufferslist = {}
+      end
+
+      local gbl = augroup('buffersl')
+      au({ 'BufWinEnter', 'WinEnter' }, {
+        group = gbl,
+        callback = function(args)
+          vim.g._bufferslist[args.buf] = vim.fn.reltimefloat(vim.fn.reltime())
+        end,
+      })
+
+      au({ 'BufDelete' }, {
+        group = gbl,
+        callback = function(args)
+          vim.g._bufferslist[args.buf] = null
+        end,
+      })
+    end,
+    config = require('plugins.configs.minipick'),
   },
 }
