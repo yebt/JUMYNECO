96c86dca39d5290cad0a908c9b9f3c028c187675
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-02 10:10:17 -0500

    feat: add better inputs

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -38,4 +38,23 @@ return {
     end,
     config = require('plugins.configs.ministarterc'),
   },
+
+  --- Inputs
+  { 'MunifTanjim/nui.nvim', module = true },
+  {
+    'stevearc/dressing.nvim',
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load({ plugins = { 'dressing.nvim' } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load({ plugins = { 'dressing.nvim' } })
+        return vim.ui.input(...)
+      end
+    end,
+    config = require('plugins.configs.dressingc'),
+  },
 }
