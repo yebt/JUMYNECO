efb11a0af14b6cac59026e2ed7e3362a0a81b2e3
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-08 10:11:45 -0500

    feat: allow checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,14 +51,14 @@ require('lazy').setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'habamax' } },
   -- automatically check for plugin updates
-  -- checker = {
-  --   -- automatically check for plugin updates
-  --   enabled = ,
-  --   concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-  --   notify = true, -- get a notification when new updates are found
-  --   frequency = 3600, -- check for updates every hour
-  --   check_pinned = false, -- check for pinned packages that can't be updated
-  -- },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = false,
