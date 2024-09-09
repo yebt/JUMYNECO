92f8574b806eae16178da03ca396647afc2ca698
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-09 08:46:28 -0500

    feat: add autocmds init for manage the lsp client attach

---
+++ lua/modules/lsp.lua
@@ -0,0 +1,8 @@
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    -- vim.bo[args.buf].formatexpr = nil
+    -- vim.bo[args.buf].omnifunc = nil
+    -- vim.keymap.del('n', 'K', { buffer = args.buf })
+  end,
+})
