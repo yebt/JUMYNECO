06428384bc80771b53441c5f09966211906a76de
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-09 08:46:38 -0500

    feat: allow tab for jump in snippets

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -20,6 +20,24 @@ return function()
       ['<C-e>'] = cmp.mapping.abort(),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.snippet.jump(1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.snippet.active({ direction = -1 }) then
+          vim.snippet.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
