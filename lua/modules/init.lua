c0a65ef68be4a3b3165f0737571b745f7681fa12
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-13 16:14:37 -0500

    feat: remap the next call when map is trigger

--- lua/modules/init.lua
+++ lua/modules/init.lua
@@ -7,14 +7,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-    --- Dependencies from the element
+    -- Create a keymap for vim.lsp.buf.implementation
+    vim.keymap.set('n', ']]', function() mlsp.words.jump(vim.v.count1, true) end, {silent =true, desc="Next lsp highliht element"})
+    vim.keymap.set('n', '[[', function() mlsp.words.jump(-vim.v.count1, true) end, {silent =true, desc="Prev lsp highliht element"})
+
+    --- Dependencies from the cli
     if not client then
       return
     end
     if client.supports_method('textDocument/documentHighlight') then
-      -- Create a keymap for vim.lsp.buf.implementation
-      vim.keymap.set('n', ']]', function() mlsp.words.jump(vim.v.count1, true) end, {silent =true, desc="Next lsp highliht element"})
-      vim.keymap.set('n', '[[', function() mlsp.words.jump(-vim.v.count1, true) end, {silent =true, desc="Prev lsp highliht element"})
     end
 
     -- vim.bo[args.buf].formatexpr = nil
