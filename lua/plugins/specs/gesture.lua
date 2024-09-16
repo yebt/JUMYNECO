8c398c6333b9505dac4871f0237cb99dd6a3ff4b
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-16 11:53:07 -0500

    feat: turn off the flash finder

--- lua/plugins/specs/gesture.lua
+++ lua/plugins/specs/gesture.lua
@@ -5,11 +5,10 @@ return {
     'folke/flash.nvim',
     event = 'VeryLazy',
     vscode = true,
-    ---@type Flash.Config
     opts = {
       modes = {
         search = {
-          enabled = true,
+          enabled = false,
         },
         char = {
           jump_labels = true,
