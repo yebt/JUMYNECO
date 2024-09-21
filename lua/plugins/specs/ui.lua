85ea60c83e701bbba6317d014f4b747343676f57
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-21 10:11:07 -0500

    feat: add fold wrapper

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -60,4 +60,28 @@ return {
     end,
     config = require('plugins.configs.dressingc'),
   },
+
+  --- Fold
+  {
+    'kevinhwang91/nvim-ufo',
+    event = 'VeryLazy',
+    dependencies = {
+      'kevinhwang91/promise-async',
+    },
+    keys = {
+      {
+        'zR',
+        function(...)
+          require('ufo').openAllFolds(...)
+        end,
+      },
+      {
+        'zM',
+        function(...)
+          require('ufo').closeAllFolds(...)
+        end,
+      },
+    },
+    config = require('plugins.configs.ufoc'),
+  },
 }
