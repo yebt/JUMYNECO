ab824e65260800655060ec523cc31a564eeed452
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-09-27 09:39:24 -0500

    fix: remove winblend for minifiles

--- lua/plugins/configs/minifilesc.lua
+++ lua/plugins/configs/minifilesc.lua
@@ -63,7 +63,7 @@ return function()
       local win_id = args.data.win_id
 
       -- Customize window-local settings
-      vim.wo[win_id].winblend = 50
+      -- vim.wo[win_id].winblend = 50
       local config = vim.api.nvim_win_get_config(win_id)
       config.border = 'single'
       config.title_pos = 'right'
