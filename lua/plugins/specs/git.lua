dcb644d0bbed4dc15022c5eaee2232566785c6b5
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-10-02 09:53:14 -0500

    feat: add mini git

--- lua/plugins/specs/git.lua
+++ lua/plugins/specs/git.lua
@@ -6,4 +6,28 @@ return {
     event = { 'VeryLazy' },
     config = require('plugins.configs.minidiffc'),
   },
+
+  --- Utils
+  {
+    'echasnovski/mini-git',
+    version = false,
+    event = { 'VeryLazy' },
+    main = 'mini.git',
+    opts = {
+      -- General CLI execution
+      job = {
+        -- Path to Git executable
+        git_executable = 'git',
+
+        -- Timeout (in ms) for each job before force quit
+        timeout = 30000,
+      },
+
+      -- Options for `:Git` command
+      command = {
+        -- Default split direction
+        split = 'auto',
+      },
+    }
+  },
 }
