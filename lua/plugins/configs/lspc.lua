25dfddbdd7e790aeb4c10f0f7b2e67353fb3ad2a
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-10-03 14:15:46 -0500

    feat: support scheme store for jsonls and yamlls

--- lua/plugins/configs/lspc.lua
+++ lua/plugins/configs/lspc.lua
@@ -70,6 +70,64 @@ return function()
         },
       }))
     end,
+
+    ['jsonls'] = function(sn)
+      lspc[sn].setup(vim.tbl_extend('force', default_server_ops, {
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      }))
+    end,
+
+    ['yamlls'] = function(sn)
+      lspc[sn].setup(vim.tbl_extend('force', default_server_ops, {
+        settings = {
+          yaml = {
+            schemaStore = {
+              -- You must disable built-in schemaStore support if you want to use
+              -- this plugin and its advanced options like `ignore`.
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = '',
+            },
+            schemas = require('schemastore').yaml.schemas(),
+          },
+        },
+      }))
+    end,
+    -- ['tailwindcss'] = function(sn)
+    --   -- lspc[sn].setup(vim.tbl_extend('force', default_server_ops, {
+    --   --   flags = { debounce_text_changes = 300 },
+    --   --   tailwindCSS = {
+    --   --     emmetCompletions = false,
+    --   --     colorDecorators = false,
+    --   --     showPixelEquivalents = false,
+    --   --     hovers = false,
+    --   --     suggestions = false,
+    --   --     codeActions = false,
+    --   --     classAttributes = { 'class', 'className', 'class:list', 'classList', 'ngClass' },
+    --   --     includeLanguages = {
+    --   --       eelixir = 'html-eex',
+    --   --       eruby = 'erb',
+    --   --       htmlangular = 'html',
+    --   --       templ = 'html',
+    --   --     },
+    --   --     lint = {
+    --   --       cssConflict = 'warning',
+    --   --       invalidApply = 'error',
+    --   --       invalidConfigPath = 'error',
+    --   --       invalidScreen = 'error',
+    --   --       invalidTailwindDirective = 'error',
+    --   --       invalidVariant = 'error',
+    --   --       recommendedVariantOrder = 'warning',
+    --   --     },
+    --   --     validate = false,
+    --   --   },
+    --   -- }))
+    -- end,
     ['emmet_language_server'] = function(sn)
       lspc[sn].setup(vim.tbl_extend('force', default_server_ops, {
         filetypes = {
