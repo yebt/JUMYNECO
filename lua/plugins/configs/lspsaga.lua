00575b7ac841d193b80630fb97cf924200b3b37c
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-10-04 14:35:57 -0500

    feat: disable lighthub in lspsaga for vistual text

--- lua/plugins/configs/lspsaga.lua
+++ lua/plugins/configs/lspsaga.lua
@@ -5,5 +5,10 @@ return function()
     symbol_in_winbar = {
       enable = false,
     },
+    lightbulb ={
+      -- enable = false,
+      sign = true,
+      virtual_text = false, -- cause an strantge error when the file is empty
+    }
   })
 end
