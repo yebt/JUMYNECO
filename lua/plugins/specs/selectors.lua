509826f56a75eb8f8880b23da2ada7292c49a967
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-10-18 10:35:19 -0500

    feat: use full files

--- lua/plugins/specs/selectors.lua
+++ lua/plugins/specs/selectors.lua
@@ -64,7 +64,7 @@ return {
       {
         '<C-p>',
         function()
-          MiniPick.builtin.files({
+          MiniPick.registry.full_files({
             -- tool = 'rg',
           }, {
             window = {
