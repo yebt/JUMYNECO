f1fd46a5b99bae4b88bb2cc380ba942460952854
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-17 12:27:49 -0500

    feat: add colors to some colors

---
+++ lua/modules/colors.lua
@@ -0,0 +1,30 @@
+-- vim.api.nvim_exec_autocmds('ColorScheme', {
+--   pattern = '*',
+--   callback = function()
+--     vim.api.nvim_set_hl_ns_fast(0)
+--     vim.api.nvim_set_hl(0, 'MatchParen', { bold = true, underline = true, reverse = true })
+--   end,
+-- })
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    vim.notify('ColorScheme')
+    vim.api.nvim_set_hl_ns_fast(0)
+    vim.api.nvim_set_hl(0, 'MatchParen', { bold = true, underline = true, reverse = true })
+    ---
+    local hi = function(name, opts)
+      opts.default = true
+      vim.api.nvim_set_hl(0, name, opts)
+    end
+
+    hi('MiniIconsAzure', { link = 'Function' })
+    hi('MiniIconsBlue', { link = 'DiagnosticInfo' })
+    hi('MiniIconsCyan', { link = 'DiagnosticHint' })
+    hi('MiniIconsGreen', { link = 'DiagnosticOk' })
+    hi('MiniIconsGrey', {})
+    hi('MiniIconsOrange', { link = 'DiagnosticWarn' })
+    hi('MiniIconsPurple', { link = 'Constant' })
+    hi('MiniIconsRed', { link = 'DiagnosticError' })
+    hi('MiniIconsYellow', { link = 'DiagnosticWarn' })
+  end,
+})
