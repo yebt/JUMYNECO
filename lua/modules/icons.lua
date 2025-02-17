0c447b96df6f0cc4704e09b215006fe714982048
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-17 12:27:55 -0500

    feat: add icons file

---
+++ lua/modules/icons.lua
@@ -0,0 +1,259 @@
+return {
+  kind = {
+    default = {
+      Text = '󰉿',
+      Method = '󰊕',
+      Function = '󰊕',
+      Constructor = '󰒓',
+
+      Field = '󰜢',
+      Variable = '󰆦',
+      Property = '󰖷',
+
+      Class = '󱡠',
+      Interface = '󱡠',
+      Struct = '󱡠',
+      Module = '󰅩',
+
+      Unit = '󰪚',
+      Value = '󰦨',
+      Enum = '󰦨',
+      EnumMember = '󰦨',
+
+      Keyword = '󰻾',
+      Constant = '󰏿',
+
+      Snippet = '󱄽',
+      Color = '󰏘',
+      File = '󰈔',
+      Reference = '󰬲',
+      Folder = '󰉋',
+      Event = '󱐋',
+      Operator = '󰪚',
+      TypeParameter = '󰬛',
+    },
+    v1 = {
+      Text = '',
+      Method = '',
+      Function = '󰡱',
+      Constructor = '',
+
+      Field = '',
+      Variable = '󰫧',
+      Property = '',
+
+      Class = '',
+      Interface = '',
+      Struct = '',
+      Module = '󰕳',
+
+      Unit = '',
+      Value = '󰎠',
+      Enum = '',
+      EnumMember = '',
+
+      Keyword = '',
+      Constant = '󰏿',
+
+      Snippet = '',
+      Color = '',
+      File = '',
+      Reference = '',
+      Folder = '',
+      Event = '',
+      Operator = '',
+      TypeParameter = '',
+    },
+    v2 = {
+      Text = '', --  (nf-fa-file_text)
+      Method = '', --  (nf-mdi-function)
+      Function = '', --  (nf-mdi-function)
+      Constructor = '', --  (nf-mdi-hammer)
+
+      Field = 'ﰠ', -- ﰠ (nf-mdi-variable)
+      Variable = '', --  (nf-mdi-variable)
+      Property = 'ﰠ', -- ﰠ (nf-mdi-variable)
+
+      Class = 'ﴯ', -- ﴯ (nf-mdi-class)
+      Interface = '', --  (nf-mdi-interface)
+      Struct = 'פּ', -- פּ (nf-mdi-struct)
+      Module = '', --  (nf-mdi-module)
+
+      Unit = '塞', -- 塞 (nf-mdi-unit)
+      Value = '', --  (nf-mdi-value)
+      Enum = '', --  (nf-mdi-enum)
+      EnumMember = '', --  (nf-mdi-enum)
+
+      Keyword = '', --  (nf-mdi-keyword)
+      Constant = '', --  (nf-mdi-constant)
+
+      Snippet = '', --  (nf-mdi-snippet)
+      Color = '', --  (nf-mdi-color)
+      File = '', --  (nf-mdi-file)
+      Reference = '', --  (nf-mdi-reference)
+      Folder = '', --  (nf-mdi-folder)
+      Event = '', --  (nf-mdi-event)
+      Operator = '', --  (nf-mdi-operator)
+      TypeParameter = '', --  (nf-mdi-type_parameter)
+    },
+    v3 = {
+      Text = '󰉿', -- nf-md-text_box (texto)
+      Method = '', -- nf-cod-symbol_method (método)
+      Function = '󰊕', -- nf-md-function (función)
+      Constructor = '', -- nf-cod-constructor (constructor)
+
+      Field = '', -- nf-cod-symbol_field (campo)
+      Variable = '', -- nf-dev-variable (variable)
+      Property = '', -- nf-custom-property (propiedad)
+
+      Class = '', -- nf-cod-symbol_class (clase)
+      Interface = '', -- nf-cod-symbol_interface (interfaz)
+      Struct = '', -- nf-cod-structure (estructura)
+      Module = '󰕳', -- nf-md-application_export (módulo)
+
+      Unit = '', -- nf-fa-cube (unidad)
+      Value = '', -- nf-mdi-numeric (valor)
+      Enum = '', -- nf-fa-list_alt (enumeración)
+      EnumMember = '', -- nf-cod-symbol_enum_member (miembro de enum)
+
+      Keyword = '', -- nf-mdi-keyword (palabra clave)
+      Constant = '', -- nf-cod-symbol_constant (constante)
+
+      Snippet = '', -- nf-mdi-code_array (snippet)
+      Color = '', -- nf-fa-paint_brush (color)
+      File = '', -- nf-fa-file (archivo)
+      Reference = '', -- nf-fa-reply (referencia)
+      Folder = '', -- nf-fa-folder (carpeta)
+      Event = '', -- nf-cod-symbol_event (evento)
+      Operator = '➕', -- nf-fa-plus (operador)
+      TypeParameter = '', -- nf-cod-type_parameter (parámetro de tipo)
+    },
+    v4 = {
+      Array = '  ',
+      Boolean = ' 󰨙 ',
+      Class = ' 󰯳 ',
+      Codeium = ' 󰘦 ',
+      Color = '  ',
+      Control = '  ',
+      Collapsed = ' > ',
+      Constant = ' 󰯱 ',
+      Constructor = '  ',
+      Copilot = '  ',
+      Enum = ' 󰯹 ',
+      EnumMember = '  ',
+      Event = '  ',
+      Field = '  ',
+      File = '  ',
+      Folder = '  ',
+      Function = ' 󰡱 ',
+      Interface = ' 󰰅 ',
+      Key = '  ',
+      Keyword = ' 󱕴 ',
+      Method = ' 󰰑 ',
+      Module = ' 󰆼 ',
+      Namespace = ' 󰰔 ',
+      Null = '  ',
+      Number = ' 󰰔 ',
+      Object = ' 󰲟 ',
+      Operator = '  ',
+      Package = ' 󰰚 ',
+      Property = ' 󰲽 ',
+      Reference = ' 󰰠 ',
+      Snippet = '  ',
+      String = '  ',
+      Struct = ' 󰰣 ',
+      TabNine = ' 󰏚 ',
+      Text = ' 󱜥 ',
+      TypeParameter = ' 󰰦 ',
+      Unit = ' 󱜥 ',
+      Value = '  ',
+      Variable = ' 󰄛 ',
+    },
+    text = {
+      Text = 'TXT',
+      Method = 'MET',
+      Function = 'FUN',
+      Constructor = 'CTR',
+
+      Field = 'FLD',
+      Variable = 'VAR',
+      Property = 'PRP',
+
+      Class = 'CLS',
+      Interface = 'ITF',
+      Struct = 'STR',
+      Module = 'MOD',
+
+      Unit = 'UNT',
+      Value = 'VAL',
+      Enum = 'ENU',
+      EnumMember = 'EMB',
+
+      Keyword = 'KEY',
+      Constant = 'CST',
+
+      Snippet = 'SNP',
+      Color = 'CLR',
+      File = 'FIL',
+      Reference = 'REF',
+      Folder = 'FOL',
+      Event = 'EVT',
+      Operator = 'OPR',
+      TypeParameter = 'TPR',
+    },
+    cmp = {
+      Text = '  ',
+      Method = '  ',
+      Function = '  ',
+      Constructor = '  ',
+      Field = '  ',
+      Variable = '  ',
+      Class = '  ',
+      Interface = '  ',
+      Module = '  ',
+      Property = '  ',
+      Unit = '  ',
+      Value = '  ',
+      Enum = '  ',
+      Keyword = '  ',
+      Snippet = '  ',
+      Color = '  ',
+      File = '  ',
+      Reference = '  ',
+      Folder = '  ',
+      EnumMember = '  ',
+      Constant = '  ',
+      Struct = '  ',
+      Event = '  ',
+      Operator = '  ',
+      TypeParameter = '  ',
+    },
+    base = {
+      Text = '',
+      Method = '󰆧',
+      Function = '󰊕',
+      Constructor = '',
+      Field = '󰇽',
+      Variable = '󰂡',
+      Class = '󰠱',
+      Interface = '',
+      Module = '',
+      Property = '󰜢',
+      Unit = '',
+      Value = '󰎠',
+      Enum = '',
+      Keyword = '󰌋',
+      Snippet = '',
+      Color = '󰏘',
+      File = '󰈙',
+      Reference = '',
+      Folder = '󰉋',
+      EnumMember = '',
+      Constant = '󰏿',
+      Struct = '',
+      Event = '',
+      Operator = '󰆕',
+      TypeParameter = '󰅲',
+    },
+  }
+}
