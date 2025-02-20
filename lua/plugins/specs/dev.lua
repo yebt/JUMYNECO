a93f4ac3546aa7a4b8b750858e31f5d118d684a8
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-20 16:31:46 -0500

    fix: add new snippets engine

--- lua/plugins/specs/dev.lua
+++ lua/plugins/specs/dev.lua
@@ -66,6 +66,40 @@ return {
       'echasnovski/mini.icons',
       -- 'moyiz/blink-emoji.nvim',
       -- 'hrsh7th/nvim-cmp',
+      -- {
+      --   'L3MON4D3/LuaSnip',
+      --   -- follow latest release.
+      --   version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      --   -- install jsregexp (optional!).
+      --   build = 'make install_jsregexp',
+      -- },
+      -- {
+      --   'echasnovski/mini.snippets',
+      --   version = '*',
+      --   dependencies = { 'rafamadriz/friendly-snippets' },
+      --   config = function()
+      --     require('mini.snippets').setup({
+      --       mappings = {
+      --         -- Expand snippet at cursor position. Created globally in Insert mode.
+      --         expand = '<C-j>',
+      --         -- Interact with default `expand.insert` session.
+      --         -- Created for the duration of active session(s)
+      --         jump_next = '<C-l>',
+      --         jump_prev = '<C-h>',
+      --         stop = '<C-c>',
+      --       },
+      --     })
+      --   end,
+      -- },
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        build = 'make install_jsregexp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load()
+        end,
+      },
     },
     cond = function(el)
       return completions == el.name
