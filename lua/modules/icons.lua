1109559a817652a858c6d3e38423997c328af907
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-20 15:00:55 -0500

    feat: add circle icons

--- lua/modules/icons.lua
+++ lua/modules/icons.lua
@@ -255,5 +255,37 @@ return {
       Operator = '󰆕',
       TypeParameter = '󰅲',
     },
+    circles = {
+      Text = '󰰥',
+      Method = '󰰐',
+      Function = '󰯻',
+      Constructor = '󰣕',
+
+      Field = '',
+      Variable = '󰄰',
+      Property = '󰁖',
+
+      Class = '󰯲',
+      Interface = '󰰄',
+      Struct = '󰰢',
+      Module = '󰘻',
+
+      Unit = '󰲠',
+      Value = '󰰫',
+      Enum = '󰸶',
+      EnumMember = '󰸷',
+
+      Keyword = '󰌆',
+      Constant = '󰏿',
+
+      Snippet = '󱍸',
+      Color = '󱠆',
+      File = '󰈔',
+      Reference = '󱆭',
+      Folder = '󰉋',
+      Event = '󰠠',
+      Operator = '󱨄',
+      TypeParameter = '󰬛',
+    }
   }
 }
