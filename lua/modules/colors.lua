0b8e9d1c231527a01168659675b787c4d4973f5d
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-24 15:33:12 -0500

    fix: remove oldcall of reload colors and add group to event

--- lua/modules/colors.lua
+++ lua/modules/colors.lua
@@ -7,23 +7,24 @@
 -- })
 vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
+  group = vim.api.nvim_create_augroup('_personal_colors', {}),
   callback = function()
-    vim.api.nvim_set_hl_ns_fast(0)
-    vim.api.nvim_set_hl(0, 'MatchParen', { bold = true, underline = true, reverse = true })
-    ---
-    local hi = function(name, opts)
-      opts.default = true
-      vim.api.nvim_set_hl(0, name, opts)
-    end
-
-    hi('MiniIconsAzure', { link = 'Function' })
-    hi('MiniIconsBlue', { link = 'DiagnosticInfo' })
-    hi('MiniIconsCyan', { link = 'DiagnosticHint' })
-    hi('MiniIconsGreen', { link = 'DiagnosticOk' })
-    hi('MiniIconsGrey', {})
-    hi('MiniIconsOrange', { link = 'DiagnosticWarn' })
-    hi('MiniIconsPurple', { link = 'Constant' })
-    hi('MiniIconsRed', { link = 'DiagnosticError' })
-    hi('MiniIconsYellow', { link = 'DiagnosticWarn' })
+    -- vim.api.nvim_set_hl_ns_fast(0)
+    -- vim.api.nvim_set_hl(0, 'MatchParen', { bold = true, underline = true, reverse = true })
+    -- ---
+    -- local hi = function(name, opts)
+    --   opts.default = true
+    --   vim.api.nvim_set_hl(0, name, opts)
+    -- end
+    --
+    -- hi('MiniIconsAzure', { link = 'Function' })
+    -- hi('MiniIconsBlue', { link = 'DiagnosticInfo' })
+    -- hi('MiniIconsCyan', { link = 'DiagnosticHint' })
+    -- hi('MiniIconsGreen', { link = 'DiagnosticOk' })
+    -- hi('MiniIconsGrey', {})
+    -- hi('MiniIconsOrange', { link = 'DiagnosticWarn' })
+    -- hi('MiniIconsPurple', { link = 'Constant' })
+    -- hi('MiniIconsRed', { link = 'DiagnosticError' })
+    -- hi('MiniIconsYellow', { link = 'DiagnosticWarn' })
   end,
 })
