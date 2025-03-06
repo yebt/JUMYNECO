05f5b7f5fad05263dbac794692129ee39cf3444f
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-03-06 10:55:00 -0500

    feat: add great new colors

--- lua/plugins/specs/colors.lua
+++ lua/plugins/specs/colors.lua
@@ -32,7 +32,13 @@
 -- local color = 'finale-nvim'
 -- local color = 'luma'
 -- local color = 'night-owl'
-local color = 'citruszest'
+-- local color = 'citruszest'
+
+--- VIMCOLORSCHEMES
+-- local color = 'ash.nvim'
+-- local color = 'oldtale.nvim'
+-- local color = 'morta'
+local color = 'vague'
 
 -- local color = 'one_monokai.nvim'
 -- local color = 'nekonight'
@@ -398,8 +404,151 @@ return {
     },
   }),
 
-  --
-  --
+  -- ash
+  makeColor({
+    'drewxs/ash.nvim',
+    name = 'ash.nvim',
+    module = 'ash',
+    colorscheme = 'ash',
+    opts = {
+      compile_path = vim.fn.stdpath('cache') .. '/ash',
+
+      transparent = false, -- transparent background
+      term_colors = true, -- terminal colors (e.g. g:terminal_color_x)
+      no_italic = false, -- disable italics
+      no_bold = false, -- disable bold
+      no_underline = false, -- disable underlines
+
+      -- override highlight groups [function/table]
+      -- e.g. highlights = function(colors)
+      --     return {
+      --         Comment = { fg = colors.red },
+      --         CmpBorder = { fg = colors.none },
+      --     }
+      -- end
+      highlights = {},
+
+      -- override style groups
+      -- e.g. comments = { "italic", "bold" }
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+    },
+  }),
+
+  -- oldtale
+  makeColor({
+    'topazape/oldtale.nvim',
+    name = 'oldtale.nvim',
+    module = 'oldtale',
+    -- colorscheme
+  }),
+
+  -- morta
+  makeColor({
+    'philosofonusus/morta.nvim',
+    name = 'morta',
+    module = 'morta',
+  }),
+
+  -- vague
+  makeColor({
+    'vague2k/vague.nvim',
+    name = 'vague',
+    module = 'vague',
+    opts = {
+      transparent = false, -- don't set background
+      style = {
+        -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+        boolean = 'bold',
+        number = 'none',
+        float = 'none',
+        error = 'bold',
+        comments = 'italic',
+        conditionals = 'none',
+        functions = 'none',
+        headings = 'bold',
+        operators = 'none',
+        strings = 'italic',
+        variables = 'none',
+
+        -- keywords
+        keywords = 'none',
+        keyword_return = 'italic',
+        keywords_loop = 'none',
+        keywords_label = 'none',
+        keywords_exception = 'none',
+
+        -- builtin
+        builtin_constants = 'bold',
+        builtin_functions = 'none',
+        builtin_types = 'bold',
+        builtin_variables = 'none',
+      },
+      -- plugin styles where applicable
+      -- make an issue/pr if you'd like to see more styling options!
+      plugins = {
+        cmp = {
+          match = 'bold',
+          match_fuzzy = 'bold',
+        },
+        dashboard = {
+          footer = 'italic',
+        },
+        lsp = {
+          diagnostic_error = 'bold',
+          diagnostic_hint = 'none',
+          diagnostic_info = 'italic',
+          diagnostic_warn = 'bold',
+        },
+        neotest = {
+          focused = 'bold',
+          adapter_name = 'bold',
+        },
+        telescope = {
+          match = 'bold',
+        },
+      },
+      -- Override colors
+      colors = {
+        bg = '#141415',
+        fg = '#cdcdcd',
+        floatBorder = '#878787',
+        line = '#252530',
+        comment = '#606079',
+        builtin = '#b4d4cf',
+        func = '#c48282',
+        string = '#e8b589',
+        number = '#e0a363',
+        property = '#c3c3d5',
+        constant = '#aeaed1',
+        parameter = '#bb9dbd',
+        visual = '#333738',
+        error = '#df6882',
+        warning = '#f3be7c',
+        hint = '#7e98e8',
+        operator = '#90a0b5',
+        keyword = '#6e94b2',
+        type = '#9bb4bc',
+        search = '#405065',
+        plus = '#8cb66d',
+        delta = '#f3be7c',
+      },
+    },
+  }),
+
+  --,
   -- --- Obscure
   -- {
   --   'killitar/obscure.nvim',
