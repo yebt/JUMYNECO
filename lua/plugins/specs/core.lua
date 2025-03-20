5298c8668790ed5095b80682b73b6ab62f772e40
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-03-20 18:51:39 -0500

    feat: remove ts commets from folke

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -28,71 +28,84 @@ return {
   },
 
   --- TS Comments
+  -- {
+  --   'folke/ts-comments.nvim',
+  --   -- lazy=false,
+  --   event = "VeryLazy",
+  --   -- dependencies={
+  --   --   'nvim-treesitter/nvim-treesitter',
+  --   -- },
+  --   opts = {
+  --     lang = {
+  --       smarty = '{* %s *}',
+  --       astro = '<!-- %s -->',
+  --       axaml = '<!-- %s -->',
+  --       blueprint = '// %s',
+  --       c = '// %s',
+  --       c_sharp = '// %s',
+  --       clojure = { ';; %s', '; %s' },
+  --       cpp = '// %s',
+  --       cs_project = '<!-- %s -->',
+  --       cue = '// %s',
+  --       fsharp = '// %s',
+  --       fsharp_project = '<!-- %s -->',
+  --       gleam = '// %s',
+  --       glimmer = '{{! %s }}',
+  --       graphql = '# %s',
+  --       handlebars = '{{! %s }}',
+  --       hcl = '# %s',
+  --       html = '<!-- %s -->',
+  --       hyprlang = '# %s',
+  --       ini = '; %s',
+  --       ipynb = '# %s',
+  --       javascript = {
+  --         '// %s',                   -- default commentstring when no treesitter node matches
+  --         '/* %s */',
+  --         call_expression = '// %s', -- specific commentstring for call_expression
+  --         jsx_attribute = '// %s',
+  --         jsx_element = '{/* %s */}',
+  --         jsx_fragment = '{/* %s */}',
+  --         spread_element = '// %s',
+  --         statement_block = '// %s',
+  --       },
+  --       kdl = '// %s',
+  --       php = '// %s',
+  --       rego = '# %s',
+  --       rescript = '// %s',
+  --       rust = { '// %s', '/* %s */' },
+  --       sql = '-- %s',
+  --       styled = '/* %s */',
+  --       svelte = '<!-- %s -->',
+  --       templ = {
+  --         '// %s',
+  --         component_block = '<!-- %s -->',
+  --       },
+  --       terraform = '# %s',
+  --       tsx = {
+  --         '// %s',                   -- default commentstring when no treesitter node matches
+  --         '/* %s */',
+  --         call_expression = '// %s', -- specific commentstring for call_expression
+  --         jsx_attribute = '// %s',
+  --         jsx_element = '{/* %s */}',
+  --         jsx_fragment = '{/* %s */}',
+  --         spread_element = '// %s',
+  --         statement_block = '// %s',
+  --       },
+  --       twig = '{# %s #}',
+  --       typescript = { '// %s', '/* %s */' }, -- langs can have multiple commentstrings
+  --       vue = '<!-- %s -->',
+  --       xaml = '<!-- %s -->',
+  --     },
+  --   },
+  -- },
+
+  -- Ts context comment
   {
-    'folke/ts-comments.nvim',
-    lazy=false,
-    -- event = 'VeryLazy',
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    event = 'VeryLazy',
     opts = {
-      lang = {
-        astro = '<!-- %s -->',
-        axaml = '<!-- %s -->',
-        blueprint = '// %s',
-        c = '// %s',
-        c_sharp = '// %s',
-        clojure = { ';; %s', '; %s' },
-        cpp = '// %s',
-        cs_project = '<!-- %s -->',
-        cue = '// %s',
-        fsharp = '// %s',
-        fsharp_project = '<!-- %s -->',
-        gleam = '// %s',
-        glimmer = '{{! %s }}',
-        graphql = '# %s',
-        handlebars = '{{! %s }}',
-        hcl = '# %s',
-        html = '<!-- %s -->',
-        hyprlang = '# %s',
-        ini = '; %s',
-        ipynb = '# %s',
-        javascript = {
-          '// %s',                   -- default commentstring when no treesitter node matches
-          '/* %s */',
-          call_expression = '// %s', -- specific commentstring for call_expression
-          jsx_attribute = '// %s',
-          jsx_element = '{/* %s */}',
-          jsx_fragment = '{/* %s */}',
-          spread_element = '// %s',
-          statement_block = '// %s',
-        },
-        kdl = '// %s',
-        php = '// %s',
-        rego = '# %s',
-        rescript = '// %s',
-        rust = { '// %s', '/* %s */' },
-        sql = '-- %s',
-        styled = '/* %s */',
-        svelte = '<!-- %s -->',
-        templ = {
-          '// %s',
-          component_block = '<!-- %s -->',
-        },
-        terraform = '# %s',
-        tsx = {
-          '// %s',                   -- default commentstring when no treesitter node matches
-          '/* %s */',
-          call_expression = '// %s', -- specific commentstring for call_expression
-          jsx_attribute = '// %s',
-          jsx_element = '{/* %s */}',
-          jsx_fragment = '{/* %s */}',
-          spread_element = '// %s',
-          statement_block = '// %s',
-        },
-        twig = '{# %s #}',
-        typescript = { '// %s', '/* %s */' }, -- langs can have multiple commentstrings
-        vue = '<!-- %s -->',
-        xaml = '<!-- %s -->',
-      },
-    },
+      -- enable_autocmd = false,
+    }
   },
 
   -- Better managment
