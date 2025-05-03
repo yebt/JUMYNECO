694ce2ac28c008b0be42eca65d927431c6217ecb
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-03 00:03:23 -0500

    fix: add opt key missing

--- lua/plugins/configs/blinkc3.lua
+++ lua/plugins/configs/blinkc3.lua
@@ -149,46 +149,48 @@ return function()
       providers = {
 
         buffer = {
-          -- keep case of first char
-          transform_items = function(a, items)
-            local keyword = a.get_keyword()
-            local correct, case
-            if keyword:match('^%l') then
-              correct = '^%u%l+$'
-              case = string.lower
-            elseif keyword:match('^%u') then
-              correct = '^%l+$'
-              case = string.upper
-            else
-              return items
-            end
-
-            -- avoid duplicates from the corrections
-            local seen = {}
-            local out = {}
-            for _, item in ipairs(items) do
-              local raw = item.insertText
-              if raw:match(correct) then
-                local text = case(raw:sub(1, 1)) .. raw:sub(2)
-                item.insertText = text
-                item.label = text
-              end
-              if not seen[item.insertText] then
-                seen[item.insertText] = true
-                table.insert(out, item)
-              end
-            end
-            return out
-          end,
-
+          --   -- keep case of first char
+          --   transform_items = function(a, items)
+          --     local keyword = a.get_keyword()
+          --     local correct, case
+          --     if keyword:match('^%l') then
+          --       correct = '^%u%l+$'
+          --       case = string.lower
+          --     elseif keyword:match('^%u') then
+          --       correct = '^%l+$'
+          --       case = string.upper
+          --     else
+          --       return items
+          --     end
           --
-          -- get_bufnrs = vim.api.nvim_list_bufs,
-
-          get_bufnrs = function()
-            return vim.tbl_filter(function(bufnr)
-              return vim.bo[bufnr].buftype == ''
-            end, vim.api.nvim_list_bufs())
-          end
+          --     -- avoid duplicates from the corrections
+          --     local seen = {}
+          --     local out = {}
+          --     for _, item in ipairs(items) do
+          --       local raw = item.insertText
+          --       if raw:match(correct) then
+          --         local text = case(raw:sub(1, 1)) .. raw:sub(2)
+          --         item.insertText = text
+          --         item.label = text
+          --       end
+          --       if not seen[item.insertText] then
+          --         seen[item.insertText] = true
+          --         table.insert(out, item)
+          --       end
+          --     end
+          --     return out
+          --   end,
+          --
+          --   --
+          --   -- get_bufnrs = vim.api.nvim_list_bufs,
+          --
+          opts = {
+            get_bufnrs = function()
+              return vim.tbl_filter(function(bufnr)
+                return vim.bo[bufnr].buftype == ''
+              end, vim.api.nvim_list_bufs())
+            end
+          }
         },
 
         lsp = { fallbacks = {} },
