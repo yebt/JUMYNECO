7096897ce35dfd6fde875f3336d926c0748961f2
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-10 19:20:04 -0500

    feat: add border single to documentation and put buffers behaviour with a writter

--- lua/plugins/configs/blinkc3.lua
+++ lua/plugins/configs/blinkc3.lua
@@ -80,7 +80,7 @@ return function()
       },
       documentation = {
         auto_show = true,
-
+        border = 'single',
         auto_show_delay_ms = 500,
         update_delay_ms = 50,
       },
@@ -162,7 +162,7 @@ return function()
         -- end,
 
         -- (optionally) always prioritize exact matches
-        -- 'exact',
+        'exact',
 
         -- pass a function for custom behavior
         -- function(item_a, item_b)
@@ -184,48 +184,40 @@ return function()
       providers = {
 
         buffer = {
-          --   -- keep case of first char
-          --   transform_items = function(a, items)
-          --     local keyword = a.get_keyword()
-          --     local correct, case
-          --     if keyword:match('^%l') then
-          --       correct = '^%u%l+$'
-          --       case = string.lower
-          --     elseif keyword:match('^%u') then
-          --       correct = '^%l+$'
-          --       case = string.upper
-          --     else
-          --       return items
-          --     end
-          --
-          --     -- avoid duplicates from the corrections
-          --     local seen = {}
-          --     local out = {}
-          --     for _, item in ipairs(items) do
-          --       local raw = item.insertText
-          --       if raw:match(correct) then
-          --         local text = case(raw:sub(1, 1)) .. raw:sub(2)
-          --         item.insertText = text
-          --         item.label = text
-          --       end
-          --       if not seen[item.insertText] then
-          --         seen[item.insertText] = true
-          --         table.insert(out, item)
-          --       end
-          --     end
-          --     return out
-          --   end,
-          --
-          --   --
-          --   -- get_bufnrs = vim.api.nvim_list_bufs,
-          --
-          -- opts = {
-          --   get_bufnrs = function()
-          --     return vim.tbl_filter(function(bufnr)
-          --       return vim.bo[bufnr].buftype == ''
-          --     end, vim.api.nvim_list_bufs())
-          --   end
-          -- }
+          buffer = {
+            -- keep case of first char
+            transform_items = function(a, items)
+              local keyword = a.get_keyword()
+              local correct, case
+              if keyword:match('^%l') then
+                correct = '^%u%l+$'
+                case = string.lower
+              elseif keyword:match('^%u') then
+                correct = '^%l+$'
+                case = string.upper
+              else
+                return items
+              end
+
+              -- avoid duplicates from the corrections
+              local seen = {}
+              local out = {}
+              for _, item in ipairs(items) do
+                local raw = item.insertText
+                if raw:match(correct) then
+                  local text = case(raw:sub(1, 1)) .. raw:sub(2)
+                  item.insertText = text
+                  item.label = text
+                end
+                if not seen[item.insertText] then
+                  seen[item.insertText] = true
+                  table.insert(out, item)
+                end
+              end
+              return out
+            end
+          }
+
         },
 
         -- lsp = { fallbacks = {} },
