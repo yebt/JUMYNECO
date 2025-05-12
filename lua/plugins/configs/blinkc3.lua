d0932f0bf0628c7906c61e8a0d93687ad423d6d6
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-11 21:53:52 -0500

    feat: restore writter buffer behaviour

--- lua/plugins/configs/blinkc3.lua
+++ lua/plugins/configs/blinkc3.lua
@@ -80,7 +80,7 @@ return function()
       },
       documentation = {
         auto_show = true,
-        border = 'single',
+        -- border = 'single',
         auto_show_delay_ms = 500,
         update_delay_ms = 50,
       },
@@ -184,41 +184,39 @@ return function()
       providers = {
 
         buffer = {
-          buffer = {
-            -- keep case of first char
-            transform_items = function(a, items)
-              local keyword = a.get_keyword()
-              local correct, case
-              if keyword:match('^%l') then
-                correct = '^%u%l+$'
-                case = string.lower
-              elseif keyword:match('^%u') then
-                correct = '^%l+$'
-                case = string.upper
-              else
-                return items
-              end
-
-              -- avoid duplicates from the corrections
-              local seen = {}
-              local out = {}
-              for _, item in ipairs(items) do
-                local raw = item.insertText
-                if raw:match(correct) then
-                  local text = case(raw:sub(1, 1)) .. raw:sub(2)
-                  item.insertText = text
-                  item.label = text
-                end
-                if not seen[item.insertText] then
-                  seen[item.insertText] = true
-                  table.insert(out, item)
-                end
-              end
-              return out
+          -- keep case of first char
+          transform_items = function(a, items)
+            local keyword = a.get_keyword()
+            local correct, case
+            if keyword:match('^%l') then
+              correct = '^%u%l+$'
+              case = string.lower
+            elseif keyword:match('^%u') then
+              correct = '^%l+$'
+              case = string.upper
+            else
+              return items
             end
-          }
 
+            -- avoid duplicates from the corrections
+            local seen = {}
+            local out = {}
+            for _, item in ipairs(items) do
+              local raw = item.insertText
+              if raw:match(correct) then
+                local text = case(raw:sub(1, 1)) .. raw:sub(2)
+                item.insertText = text
+                item.label = text
+              end
+              if not seen[item.insertText] then
+                seen[item.insertText] = true
+                table.insert(out, item)
+              end
+            end
+            return out
+          end
         },
+
 
         -- lsp = { fallbacks = {} },
         --
