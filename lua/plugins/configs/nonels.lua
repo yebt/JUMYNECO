48f5dc4d96066948e8b807c52efa42498519ed64
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-13 10:32:16 -0500

    feat: format

--- lua/plugins/configs/nonels.lua
+++ lua/plugins/configs/nonels.lua
@@ -1,7 +1,7 @@
 return function()
-  local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  -- local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  -- local msn = require('mason')
 
-  local msn = require('mason')
   local msnnls = require('mason-null-ls')
   local nls = require('null-ls')
   local util = vim.lsp.util
@@ -80,120 +80,120 @@ return function()
     -- end,
   })
 
-  local function overwrite_format2(options)
-    options = options or {
-      -- timeout_ms = 3000,
-    }
-    local bufnr = options.bufnr or api.nvim_get_current_buf()
-    local method = 'textDocument/formatting'
-    -- local clients = vim.lsp.get_active_clients({
-    --   id = options.id,
-    --   bufnr = bufnr,
-    --   name = options.name,
-    --   method = method,
-    -- })
-
-    local clients = vim.lsp.get_clients({
-      id = options.id,
-      bufnr = bufnr,
-      name = options.name,
-      method = method,
-    })
-
-    if options.filter then
-      clients = vim.tbl_filter(options.filter, clients)
-    end
-    -- filer if client.server_capabilities.documentFormattingProvider
-    clients = vim.tbl_filter(function(client)
-      return client.server_capabilities.documentFormattingProvider
-    end, clients)
-
-    if #clients == 0 then
-      vim.notify('[LSP] Format request failed, no matching language servers.')
-    end
-
-    local copyClient = vim.deepcopy(clients)
-    table.insert(copyClient, { name = 'all' })
-    table.insert(copyClient, { name = 'Set default formatter' })
-
-    --@private
-    local function applyFormat(clientsi)
-      if options.async then
-        local do_format
-        do_format = function(idx, client)
-          if not client then
-            return
-          end
-          -- local params = set_range(client, util.make_formatting_params(options.formatting_options))
-          local params = util.make_formatting_params(options.formatting_options)
-          client.request(method, params, function(...)
-            local handler = client.handlers[method] or vim.lsp.handlers[method]
-            handler(...)
-            do_format(next(clientsi, idx))
-          end, bufnr)
-        end
-        do_format(next(clientsi))
-      else
-        local timeout_ms = options.timeout_ms or 1000
-        for _, client in pairs(clientsi) do
-          local params = util.make_formatting_params(options.formatting_options)
-          local result, err = client.request_sync(method, params, timeout_ms, bufnr)
-          if result and result.result then
-            util.apply_text_edits(result.result, bufnr, client.offset_encoding)
-          end
-          if err then
-            vim.notify(string.format('[LSP][%s] %s', client.name, err), vim.log.levels.WARN)
-          end
-        end
-      end
-    end
-
-    -- check if formatter is set
-    if vim.g.defaultFormatClient then
-      -- check if formatter is in list
-      for _, client in pairs(clients) do
-        if client.name == vim.g.defaultFormatClient then
-          applyFormat({ client })
-          return
-        end
-      end
-      vim.notify('Default Formatter not found: ' .. vim.g.defaultFormatClient)
-    end
-
-    if #clients > 1 then
-      vim.ui.select(copyClient, {
-        prompt = 'Select a formatter:',
-        format_item = function(item)
-          return item.name
-        end,
-      }, function(choice)
-        if not choice then
-          vim.notify('No formatter selected')
-        elseif choice.name == 'all' then
-          applyFormat(clients)
-        elseif choice.name == 'Set default formatter' then
-          vim.ui.select(clients, {
-            prompt = 'Select a formatter:',
-            format_item = function(item)
-              return item.name
-            end,
-          }, function(choicei)
-            if not choicei then
-              vim.notify('No formatter selected')
-            else
-              vim.notify('Set default formatter:' .. choicei.name)
-              vim.g.defaultFormatClient = choicei.name
-              applyFormat({ choicei })
-            end
-          end)
-        elseif choice then
-          applyFormat({ choice })
-        end
-      end)
-    else
-      applyFormat(clients)
-    end
-  end
+  -- local function overwrite_format2(options)
+  --   options = options or {
+  --     -- timeout_ms = 3000,
+  --   }
+  --   local bufnr = options.bufnr or api.nvim_get_current_buf()
+  --   local method = 'textDocument/formatting'
+  --   -- local clients = vim.lsp.get_active_clients({
+  --   --   id = options.id,
+  --   --   bufnr = bufnr,
+  --   --   name = options.name,
+  --   --   method = method,
+  --   -- })
+  --
+  --   local clients = vim.lsp.get_clients({
+  --     id = options.id,
+  --     bufnr = bufnr,
+  --     name = options.name,
+  --     method = method,
+  --   })
+  --
+  --   if options.filter then
+  --     clients = vim.tbl_filter(options.filter, clients)
+  --   end
+  --   -- filer if client.server_capabilities.documentFormattingProvider
+  --   clients = vim.tbl_filter(function(client)
+  --     return client.server_capabilities.documentFormattingProvider
+  --   end, clients)
+  --
+  --   if #clients == 0 then
+  --     vim.notify('[LSP] Format request failed, no matching language servers.')
+  --   end
+  --
+  --   local copyClient = vim.deepcopy(clients)
+  --   table.insert(copyClient, { name = 'all' })
+  --   table.insert(copyClient, { name = 'Set default formatter' })
+  --
+  --   --@private
+  --   local function applyFormat(clientsi)
+  --     if options.async then
+  --       local do_format
+  --       do_format = function(idx, client)
+  --         if not client then
+  --           return
+  --         end
+  --         -- local params = set_range(client, util.make_formatting_params(options.formatting_options))
+  --         local params = util.make_formatting_params(options.formatting_options)
+  --         client.request(method, params, function(...)
+  --           local handler = client.handlers[method] or vim.lsp.handlers[method]
+  --           handler(...)
+  --           do_format(next(clientsi, idx))
+  --         end, bufnr)
+  --       end
+  --       do_format(next(clientsi))
+  --     else
+  --       local timeout_ms = options.timeout_ms or 1000
+  --       for _, client in pairs(clientsi) do
+  --         local params = util.make_formatting_params(options.formatting_options)
+  --         local result, err = client.request_sync(method, params, timeout_ms, bufnr)
+  --         if result and result.result then
+  --           util.apply_text_edits(result.result, bufnr, client.offset_encoding)
+  --         end
+  --         if err then
+  --           vim.notify(string.format('[LSP][%s] %s', client.name, err), vim.log.levels.WARN)
+  --         end
+  --       end
+  --     end
+  --   end
+  --
+  --   -- check if formatter is set
+  --   if vim.g.defaultFormatClient then
+  --     -- check if formatter is in list
+  --     for _, client in pairs(clients) do
+  --       if client.name == vim.g.defaultFormatClient then
+  --         applyFormat({ client })
+  --         return
+  --       end
+  --     end
+  --     vim.notify('Default Formatter not found: ' .. vim.g.defaultFormatClient)
+  --   end
+  --
+  --   if #clients > 1 then
+  --     vim.ui.select(copyClient, {
+  --       prompt = 'Select a formatter:',
+  --       format_item = function(item)
+  --         return item.name
+  --       end,
+  --     }, function(choice)
+  --       if not choice then
+  --         vim.notify('No formatter selected')
+  --       elseif choice.name == 'all' then
+  --         applyFormat(clients)
+  --       elseif choice.name == 'Set default formatter' then
+  --         vim.ui.select(clients, {
+  --           prompt = 'Select a formatter:',
+  --           format_item = function(item)
+  --             return item.name
+  --           end,
+  --         }, function(choicei)
+  --           if not choicei then
+  --             vim.notify('No formatter selected')
+  --           else
+  --             vim.notify('Set default formatter:' .. choicei.name)
+  --             vim.g.defaultFormatClient = choicei.name
+  --             applyFormat({ choicei })
+  --           end
+  --         end)
+  --       elseif choice then
+  --         applyFormat({ choice })
+  --       end
+  --     end)
+  --   else
+  --     applyFormat(clients)
+  --   end
+  -- end
 
   local function overwrite_format3(opts)
     opts = opts or {}
@@ -201,11 +201,11 @@ return function()
     local buffer_filetype = vim.bo.filetype
     local default_frmtr_client = vim.g['default_formatter_' .. buffer_filetype] or nil
 
-    local frmt_lsp_method = 'textDocument/formatting';
+    local frmt_lsp_method = 'textDocument/formatting'
 
     local frmt_lsp_clients = vim.lsp.get_clients({
       bufnr = 0, -- actual buffer
-      method = frmt_lsp_method
+      method = frmt_lsp_method,
     })
 
     if #frmt_lsp_clients == 0 then
@@ -218,31 +218,31 @@ return function()
       local def_client_name = default_frmtr_client
       local def_client = vim.lsp.get_clients({ name = def_client_name })[1] or nil
       if def_client then
-        vim.lsp.buf.format {
+        vim.lsp.buf.format({
           buf = 0,
           async = true,
-          id = def_client.id
-        }
+          id = def_client.id,
+        })
         return
       end
     end
 
     local copy_cliets = vim.deepcopy(frmt_lsp_clients)
     table.insert(copy_cliets, { special = 'ALL', name = 'All' })
-    table.insert(copy_cliets, { special = 'SAD', name = "Select a default" })
+    table.insert(copy_cliets, { special = 'SAD', name = 'Select a default' })
 
     local function applyFormat(allow_default)
-      if (allow_default == nil) then
+      if allow_default == nil then
         allow_default = true
       end
 
       local frmt_lsp_clients_to_select = allow_default and copy_cliets or frmt_lsp_clients
 
       vim.ui.select(frmt_lsp_clients_to_select, {
-        prompt = "Select a fomatter:",
+        prompt = 'Select a fomatter:',
         format_item = function(itm)
           return itm.name
-        end
+        end,
       }, function(choise)
         if choise then
           if choise.special == 'All' then
@@ -250,7 +250,7 @@ return function()
               bufnr = 0,
               async = true,
             })
-          elseif choise.special == "SAD" then
+          elseif choise.special == 'SAD' then
             applyFormat(false)
           else
             if not allow_default then
@@ -263,7 +263,7 @@ return function()
             vim.lsp.buf.format({
               bufnr = 0,
               async = true,
-              id = choise.id
+              id = choise.id,
             })
           end
           -- if choise.
