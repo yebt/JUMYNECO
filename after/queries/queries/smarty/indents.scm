3e771faebe086541587a18c56d4053d1fe9a3ede
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-22 11:52:03 -0500

    feat: add quieries

---
+++ after/queries/queries/smarty/indents.scm
@@ -0,0 +1,21 @@
+[
+ (if)
+ (foreach)
+ (block)
+ (nocache)
+] @indent.begin
+
+[
+ (else_if)
+ (else)
+ (foreach_else)
+] @indent.branch
+
+[
+ "{/if}"
+ "{/foreach}"
+ "{/block}"
+ "{/nocache}"
+] @indent.end
+
+(ERROR) @indent.auto
