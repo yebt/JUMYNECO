2de25ce87c6ab28dc7ecd3a2bcc012cf74edc670
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-23 11:19:35 -0500

    feat: make a new struct

--- init.lua
+++ init.lua
@@ -8,11 +8,31 @@
 -- Allow experimental loading
 vim.loader.enable()
 
+--- Flag
+local lazy_autocmds = vim.fn.argc(-1) == 0
+
 -- Set leaders
 vim.g.mapleader = vim.keycode('<space>')
 vim.g.maplocal = vim.keycode('<space>')
 
+-- Base
 require("configs.options")
+require("configs.strive")
 
--- vim.cmd.colorscheme('habamax')
--- vim.cmd.colorscheme('solarized')
+if not lazy_autocmds then
+  require("configs.aus")
+end
+
+--- NOTE: event User StriveDone
+vim.api.nvim_create_autocmd('User',{
+  group = vim.api.nvim_create_augroup('StrivePostLoad', { clear = false }),
+  once = true,
+  callback = function()
+    if lazy_autocmds then
+      require("configs.aus")
+    end
+    require("configs.kmps")
+  end,
+  desc="Actios after load strive"
+})
+
---
+++ lua/configs/strive.lua
@@ -0,0 +1,12 @@
+local strive_path = vim.fn.stdpath('data') .. '/site/pack/strive/start/strive'
+if not vim.uv.fs_stat(strive_path) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--depth=1',
+    'https://github.com/nvimdev/strive',
+    strive_path
+  })
+
+  vim.o.rtp = strive_path .. ',' .. vim.o.rtp
+end
