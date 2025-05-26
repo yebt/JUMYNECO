8ad3bcd0af149bb678754b5fb6ef2ee567e8e5ab
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-26 17:25:35 -0500

    feat: add fash to work

--- lua/plugins/specs/utils.lua
+++ lua/plugins/specs/utils.lua
@@ -42,4 +42,143 @@ return {
       },
     },
   },
+
+
+  --- Flash
+  {
+    "folke/flash.nvim",
+    -- event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      -- search = { forward = true, wrap = false, multi_window = false },
+    },
+    -- stylua: ignore
+    keys = {
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      {
+        "<leader>lD",
+        function()
+          -- More advanced example that also highlights diagnostics:
+          require("flash").jump({
+            matcher = function(win)
+              ---@param diag Diagnostic
+              return vim.tbl_map(function(diag)
+                return {
+                  pos = { diag.lnum + 1, diag.col },
+                  end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
+                }
+              end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
+            end,
+            action = function(match, state)
+              vim.api.nvim_win_call(match.win, function()
+                vim.api.nvim_win_set_cursor(match.win, match.pos)
+                vim.diagnostic.open_float()
+              end)
+              state:restore()
+            end,
+          })
+        end,
+        desc ="Show diagnostics at target, without changing cursor position"
+      },
+      {
+        "<leader>ld",
+        function()
+          -- More advanced example that also highlights diagnostics:
+          require("flash").jump({
+            matcher = function(win)
+              ---@param diag Diagnostic
+              return vim.tbl_map(function(diag)
+                return {
+                  pos = { diag.lnum + 1, diag.col },
+                  end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
+                }
+              end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
+            end,
+            action = function(match, state)
+              vim.api.nvim_win_call(match.win, function()
+                vim.api.nvim_win_set_cursor(match.win, match.pos)
+                vim.diagnostic.open_float()
+              end)
+              state:restore()
+            end,
+          })
+        end,
+        desc ="Show diagnostics at target, without changing cursor position"
+      },
+      {
+        "<leader><leader>",
+        function()
+          require("flash").jump({
+            pattern = ".", -- initialize pattern with any char
+            search = {
+              mode = function(pattern)
+                -- remove leading dot
+                if pattern:sub(1, 1) == "." then
+                  pattern = pattern:sub(2)
+                end
+                -- return word pattern and proper skip pattern
+                return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+              end,
+            },
+            -- select the range
+            jump = { pos = "range" },
+          })
+        end,
+        desc = "Select any word"
+      },
+      {
+        "<leader>f",
+        function()
+          local Flash = require("flash")
+
+          ---@param opts Flash.Format
+          local function format(opts)
+            -- always show first and second label
+            return {
+              { opts.match.label1, "FlashMatch" },
+              { opts.match.label2, "FlashLabel" },
+            }
+          end
+
+          Flash.jump({
+            search = { mode = "search" },
+            label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+            pattern = [[\<]],
+            action = function(match, state)
+              state:hide()
+              Flash.jump({
+                search = { max_length = 0 },
+                highlight = { matches = false },
+                label = { format = format },
+                matcher = function(win)
+                  -- limit matches to the current label
+                  return vim.tbl_filter(function(m)
+                    return m.label == match.label and m.win == win
+                  end, state.results)
+                end,
+                labeler = function(matches)
+                  for _, m in ipairs(matches) do
+                    m.label = m.label2 -- use the second label
+                  end
+                end,
+              })
+            end,
+            labeler = function(matches, state)
+              local labels = state:labels()
+              for m, match in ipairs(matches) do
+                match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                match.label2 = labels[(m - 1) % #labels + 1]
+                match.label = match.label1
+              end
+            end,
+          })
+        end,
+        desc = "2 Char jump"
+      }
+    },
+  }
 }
