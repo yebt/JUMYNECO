4e49ba1f10fa61a700a8f0ebfba8f52d0f38b383
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-26 11:30:46 -0500

    feat: init a config for future files

---
+++ lua/plugins/configs/mini-files-c.lua
@@ -0,0 +1,9 @@
+return function()
+  --- Minifiles use the rename of the Snacks
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesActionRename",
+    callback = function(event)
+      Snacks.rename.on_rename_file(event.data.from, event.data.to)
+    end,
+  })
+end
