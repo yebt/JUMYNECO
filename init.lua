5ec25baffda84dd117c469d452d5842d36fe32cc
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-26 11:30:37 -0500

    feat add new event for postload, and profiles from snaks

--- init.lua
+++ init.lua
@@ -5,6 +5,20 @@
 --- Lotus nvim by: yebt
 ------------------------------
 
+if vim.env.PROF then
+  -- example for lazy.nvim
+  -- change this to the correct path for your plugin manager
+  local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
+  vim.opt.rtp:append(snacks)
+  require("snacks.profiler").startup({
+    startup = {
+      event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  })
+end
+
 require('config.options')
 
 vim.g.mapleader = vim.keycode('<space>')
@@ -28,8 +42,13 @@ vim.api.nvim_create_autocmd('User', {
     require('config.kmaps')
     -- lsp configs local
     require('config.lspl')
+    vim.api.nvim_exec_autocmds('User',{
+      pattern = 'PostVeryLazy'
+    })
   end,
 })
+
+--- FIX: remove it:
 
 -- vim.api.nvim_create_autocmd('User', {
 --   pattern="LazyFile",
@@ -45,3 +64,5 @@ vim.api.nvim_create_autocmd('User', {
 -- })
 
 require('config.lazy')
+--- Startup times for process: Primary (or UI client) ---
+
