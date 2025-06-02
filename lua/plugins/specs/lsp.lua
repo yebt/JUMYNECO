375866f35d1f5b43ff22bc359ab8c156dc9d066c
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-02 10:54:19 -0500

    feat: add lsp init spec

---
+++ lua/plugins/specs/lsp.lua
@@ -0,0 +1,31 @@
+--- Here is all tooling to interactuate with LSP
+
+return  {
+  --- List all diagnostics
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    dependencies = {
+      {
+        "folke/which-key.nvim",
+        opts = {
+          spec = {
+            {
+              {
+                mode={'n'},
+                {'<leader>x', group = "Trouble", icon = "󱍼"}
+              }
+            }
+          },
+        }
+      }
+    },
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>xS", "<cmd>Trouble lsp toggle<cr>", desc = "LSP references/definitions/... (Trouble)" },
+    }
+  },
+}
