99e669c23536c21da667da8ca75f01feaee69713
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-03 14:02:48 -0500

    feat: add colors to support hexs view

--- lua/plugins/specs/colors.lua
+++ lua/plugins/specs/colors.lua
@@ -1,3 +1,4 @@
+--- #ababab
 --- COLORS THINGS
 --- Just the colors things
 -- 'tokyonight.nvim', 'slack.nvim'
@@ -59,4 +60,27 @@ return {
       vim.cmd.colorscheme('slack')
     end
   },
+
+  {
+    'echasnovski/mini.hipatterns',
+    version = false,
+    event = 'VeryLazy',
+    -- opts = {}
+    config = function()
+      local hipatterns = require('mini.hipatterns')
+      hipatterns.setup({
+        highlighters = {
+          -- -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          -- fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          -- hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+          -- todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+          -- note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      })
+    end
+  },
+
+
 }
