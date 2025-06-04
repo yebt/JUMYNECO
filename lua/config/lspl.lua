0165db09a71253762449e6c8f2402c58ac94ec0b
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-03 22:38:38 -0500

    feat: add settings for vim.lsp.config

--- lua/config/lspl.lua
+++ lua/config/lspl.lua
@@ -1,10 +1,32 @@
 -- OFF the log of lsp
-vim.lsp.log.set_level(vim.log.levels.OFF)
+-- vim.lsp.log.set_level(vim.log.levels.OFF)
+local icons = {
+  Error = 'E',
+  Warn = 'W',
+  Info = 'I',
+  Hint = 'H'
+}
 
 --- Diagnostic configs
 vim.diagnostic.config({
-  virtual_text = { current_line = true },
+  virtual_text = {
+    current_line = true,
+    spacing = 4,
+    source = "if_many",
+    -- prefix = "●",
+  },
+  virtual_lines ={
+    current_line = true,
+  },
+  underline = true,
+  update_in_insert = false,
   signs = {
-    text = { '●', '●', '●', '●' },
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.Error,
+      [vim.diagnostic.severity.WARN] = icons.Warn,
+      [vim.diagnostic.severity.HINT] = icons.Hint,
+      [vim.diagnostic.severity.INFO] = icons.Info
+
+    },
   },
 })
--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -74,4 +74,5 @@ return {
       "LspUninstall"
     }
   },
+
 }
