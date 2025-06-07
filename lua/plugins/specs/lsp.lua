c48d3a40512783b621071ca80d5ef04a228bb8e0
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-07 01:00:22 -0500

    feat: add mason for fomratter

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -84,4 +84,42 @@ return {
     }
   },
 
+  --- None ls Format and lintters
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "mason-org/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          ensure_installed = { "stylua", "jq" }
+        },
+        automatic_installation = false,
+        handlers = {},
+      })
+      require("null-ls").setup({
+        sources = {
+        }
+      })
+    end,
+  },
+
+  --- LSP Saga
+
+  --- Formatt and Lint
+  -- {
+  --   "nvimdev/guard.nvim",
+  --   event = {
+  --     "VeryLazy",
+  --     -- "LazyFile"
+  --   },
+  --   dependencies = {
+  --     "nvimdev/guard-collection"
+  --   },
+  --   config = require("plugins.configs.guard-c")
+  -- },
 }
