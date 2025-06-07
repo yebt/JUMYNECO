d4de38cd2e0cebc3ccd9efaff105ee3168d34b20
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-07 11:10:16 -0500

    feat: add licence to intelephense

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,4 +1,10 @@
 return {
+  init_options = {
+    -- storagePath = …, -- Optional absolute path to storage dir. Defaults to os.tmpdir().
+    -- globalStoragePath = …, -- Optional absolute path to a global storage dir. Defaults to os.homedir().
+    licenceKey = "~/intelephense/licence.txt", -- Optional licence key or absolute path to a text file containing the licence key.
+    -- clearCache = …, -- Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
+  },
   settings = {
     intelephense = {
       diagnostics = {
