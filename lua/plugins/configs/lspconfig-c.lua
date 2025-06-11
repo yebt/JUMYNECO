9691b68a8693a6ebbdd413219185d3dea1cabd73
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-11 14:30:02 -0500

    feat: load dinamically files from lspconfig internal serttings

--- lua/plugins/configs/lspconfig-c.lua
+++ lua/plugins/configs/lspconfig-c.lua
@@ -1,8 +1,20 @@
 return function ()
 
-  vim.print('---------')
-vim.lsp.enable('vtsls')
-vim.lsp.config('vtsls',{
-  cmd = {'echo ', "ls"}
-})
+  local lspconfigsdir = vim.fn.stdpath 'config' .. "/lua/config/lspconfigs"
+
+  local function ends_with(str, ending)
+    return ending == "" or str:sub(-#ending) == ending
+  end
+
+  -- Iterate through files in the directory
+  for _, file in ipairs(vim.fn.readdir(lspconfigsdir)) do
+
+    if ends_with(file, '.lua') then
+      local mod = file:gsub('%.lua$', '')   -- Remove .lua extension
+      local server_name = mod
+      local server_settings = require('config.lspconfigs.'..mod)
+      vim.lsp.enable(server_name)
+      vim.lsp.config(server_name,server_settings)
+    end
+  end
 end
