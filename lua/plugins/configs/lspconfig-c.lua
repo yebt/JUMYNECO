7c74abd05b0b75fe98d659fd1ed088547972bd58
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-12 16:00:03 -0500

    fix: allow completion in phpdoc for intelephense

--- lua/plugins/configs/lspconfig-c.lua
+++ lua/plugins/configs/lspconfig-c.lua
@@ -92,7 +92,13 @@ return function()
           format = {
             enable = true,
           },
+          phpdoc = { textFormat = 'snippet' },             -- asegura snippet output
+          completion = {
+            fullyQualifyGlobalConstantsAndFunctions = true
+          },
         },
+        -- phpdoc = { textFormat = 'snippet' },             -- asegura snippet output
+        -- completion = { fullyQualifyGlobalConstantsAndFunctions = true },
       },
     },
     ['lua_ls'] = {
@@ -110,145 +116,145 @@ return function()
             -- library = vim.api.nvim_get_runtime_file("", true)
           },
           -- diagnostics = {
-          --   globals = {
-          --     'vim',
-          --   },
-          -- },
-        },
-      },
-    },
-    ['pyright'] = {
-      settings = {
-        python = {
-          analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = 'openFilesOnly',
-            useLibraryCodeForTypes = true,
+            --   globals = {
+              --     'vim',
+              --   },
+              -- },
+            },
           },
         },
-      },
-    },
-    --- NOTE: OLD ts server, now i use vtsls
-    ['ts_ls'] = {
-      filetypes = {
-        'javascript',
-        'typescript',
-        'vue',
-      },
-      init_options = {
-        plugins = {
-          {
-            name = '@vue/typescript-plugin',
-            location = vim.fn.stdpath('data')
-              .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-            languages = { 'vue' },
-          },
-        },
-      },
-      settings = {
-        typescript = {
-          tsserver = {
-            useSyntaxServer = false,
-          },
-          inlayHints = {
-            includeInlayParameterNameHints = 'all',
-            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
-        },
-      },
-    },
-    ['vtsls'] = {
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'javascript.jsx',
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
-        'vue',
-      },
-      settings = {
-        vtsls = {
-          tsserver = {
-            globalPlugins = {
-              --- Used fot typescript in vue projects
-              {
-                name = '@vue/typescript-plugin',
-                location = vim.fn.stdpath('data')
-                  .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                languages = { 'vue' },
-                configNamespace = 'typescript',
-                enableForWorkspaceTypeScriptVersions = true,
+        ['pyright'] = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = 'openFilesOnly',
+                useLibraryCodeForTypes = true,
               },
             },
           },
         },
-      },
-      root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
-    },
-    ['vue_ls'] = {
-      -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-      init_options = {
-        vue = {
-          -- disable hybrid mode, this dont need ts_ls with vue ts plugni
-          -- hybridMode = false,
-
-          hybridMode = true, -- Use vtsls to manage typescript
-        },
-        -- typescript = {
-        --   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-        -- },
-      },
-    },
-    ['jsonls'] = {
-      settings = {
-        json = {
-          schemas = require('schemastore').json.schemas(),
-          -- schemas = jschemas,
-          validate = { enable = true },
-        },
-      },
-    },
-    ['yamlls'] = {
-      settings = {
-        yaml = {
-          schemaStore = {
-            -- You must disable built-in schemaStore support if you want to use
-            -- this plugin and its advanced options like `ignore`.
-            enable = false,
-            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-            url = '',
+        --- NOTE: OLD ts server, now i use vtsls
+        ['ts_ls'] = {
+          filetypes = {
+            'javascript',
+            'typescript',
+            'vue',
           },
-          schemas = require('schemastore').yaml.schemas(),
+          init_options = {
+            plugins = {
+              {
+                name = '@vue/typescript-plugin',
+                location = vim.fn.stdpath('data')
+                .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                languages = { 'vue' },
+              },
+            },
+          },
+          settings = {
+            typescript = {
+              tsserver = {
+                useSyntaxServer = false,
+              },
+              inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
         },
-      },
-    },
-  }
+        ['vtsls'] = {
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'javascript.jsx',
+            'typescript',
+            'typescriptreact',
+            'typescript.tsx',
+            'vue',
+          },
+          settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  --- Used fot typescript in vue projects
+                  {
+                    name = '@vue/typescript-plugin',
+                    location = vim.fn.stdpath('data')
+                    .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                    languages = { 'vue' },
+                    configNamespace = 'typescript',
+                    enableForWorkspaceTypeScriptVersions = true,
+                  },
+                },
+              },
+            },
+          },
+          root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
+        },
+        ['vue_ls'] = {
+          -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+          init_options = {
+            vue = {
+              -- disable hybrid mode, this dont need ts_ls with vue ts plugni
+              -- hybridMode = false,
 
-  for srvr, srvr_settings in pairs(lsp_settings) do
-    vim.lsp.config(srvr, srvr_settings)
-  end
-  -- local lspconfigsdir = vim.fn.stdpath 'config' .. "/lua/config/lspconfigs"
-  --
-  -- local function ends_with(str, ending)
-  --   return ending == "" or str:sub(-#ending) == ending
-  -- end
-  --
-  -- -- Iterate through files in the directory
-  -- for _, file in ipairs(vim.fn.readdir(lspconfigsdir)) do
-  --
-  --   if ends_with(file, '.lua') then
-  --     local mod = file:gsub('%.lua$', '')   -- Remove .lua extension
-  --     local server_name = mod
-  --     local server_settings = require('config.lspconfigs.'..mod)
-  --     vim.lsp.enable(server_name)
-  --     vim.lsp.config(server_name,server_settings)
-  --   end
-  -- end
-end
+              hybridMode = true, -- Use vtsls to manage typescript
+            },
+            -- typescript = {
+              --   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+              -- },
+            },
+          },
+          ['jsonls'] = {
+            settings = {
+              json = {
+                schemas = require('schemastore').json.schemas(),
+                -- schemas = jschemas,
+                validate = { enable = true },
+              },
+            },
+          },
+          ['yamlls'] = {
+            settings = {
+              yaml = {
+                schemaStore = {
+                  -- You must disable built-in schemaStore support if you want to use
+                  -- this plugin and its advanced options like `ignore`.
+                  enable = false,
+                  -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                  url = '',
+                },
+                schemas = require('schemastore').yaml.schemas(),
+              },
+            },
+          },
+        }
+
+        for srvr, srvr_settings in pairs(lsp_settings) do
+          vim.lsp.config(srvr, srvr_settings)
+        end
+        -- local lspconfigsdir = vim.fn.stdpath 'config' .. "/lua/config/lspconfigs"
+        --
+        -- local function ends_with(str, ending)
+          --   return ending == "" or str:sub(-#ending) == ending
+          -- end
+          --
+          -- -- Iterate through files in the directory
+          -- for _, file in ipairs(vim.fn.readdir(lspconfigsdir)) do
+          --
+          --   if ends_with(file, '.lua') then
+          --     local mod = file:gsub('%.lua$', '')   -- Remove .lua extension
+          --     local server_name = mod
+          --     local server_settings = require('config.lspconfigs.'..mod)
+          --     vim.lsp.enable(server_name)
+          --     vim.lsp.config(server_name,server_settings)
+          --   end
+          -- end
+        end
