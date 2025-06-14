83a94618970182a0344253c70860cf7d122ec674
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-06-14 10:17:15 -0500

    feat: add git conflic visualizer an selector

--- lua/plugins/specs/git.lua
+++ lua/plugins/specs/git.lua
@@ -24,8 +24,8 @@ return {
       },
       signs_staged_enable = true,
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
         follow_files = true,
@@ -44,7 +44,7 @@ return {
       current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
       sign_priority = 6,
       update_debounce = 100,
-      status_formatter = nil, -- Use default
+      status_formatter = nil,  -- Use default
       max_file_length = 40000, -- Disable if file is longer than this (in lines)
       preview_config = {
         -- Options passed to nvim_open_win
@@ -106,6 +106,20 @@ return {
         },
       },
     },
+  },
+
+  {
+    "akinsho/git-conflict.nvim",
+    -- event = { "VeryLazy" },
+    cmd = {
+      "GitConflictChooseOurs",
+      "GitConflictChooseTheirs",
+      "GitConflictChooseBoth",
+      "GitConflictChooseNone",
+      "GitConflictNextConflict",
+      "GitConflictPrevConflict",
+      "GitConflictListQf",
+    }
   },
 
   -- DIFF
