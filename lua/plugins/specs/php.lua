b15f7f8433aac01a0a298845da48a68c3cd1c4af
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-07-30 10:01:15 -0500

    feat: add php specs

---
+++ lua/plugins/specs/php.lua
@@ -0,0 +1,14 @@
+return {
+  -- {
+  --   "camilledejoye/phpactor-mappings",
+  --   -- event = { 'VeryLazy' },
+  --   ft = {"php"}
+  -- }
+
+  -- {
+  --   "phpactor/phpactor",
+  --   ft = {"php"},
+  --   -- tag = "*",
+  --   build = "composer install --no-dev -o",
+  -- },
+}
